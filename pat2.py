62d46cb0608069da9d675b37f5f86dfbcf13658b
Anshika <anshikayadav8500@gmail.com>
2023-07-03 14:03:09 +0530

    list

--- pat2.py
+++ pat2.py
@@ -1,12 +1,13 @@
 from turtle import *
 speed('fastest')
 bgcolor('black')
-colors = ['red','orange','yellow','green','blue','purple']
+colors = ['red','orange','yellow','green','blue']
 
 i = 0
 while True:
-    pencolor(colors[i%6])
+    pencolor(colors[i%5])
     fd(10+i)
-    lt(61)
+    lt(200)
+
     i += 1
 mainloop()
